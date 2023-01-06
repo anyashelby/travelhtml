0357218f0051d5dbdb18a5ee78dfa48144c58db3
Andrey <andreyhlvn@gmail.com>
2023-01-06 11:38:38 +0100

    finished responsive on 850px

--- js/main.js
+++ js/main.js
@@ -1,21 +1,39 @@
 //carousel
 const owl = $('.owl-carousel');
 owl.owlCarousel({
-    center: true,
-    loop: true,
-    margin: 30,
-    startPosition: 1,
-    items: 3,
+	center: true,
+	loop: true,
+	margin: 30,
+	startPosition: 1,
+	items: 3,
+	responsive : {
+		
+		0 : {
+			
+		},
+		
+		480 : {
+			
+		},
+		
+		1000 : {
+			margin: 20,	
+		},
+
+		1200 : {
+			margin: 30,
+		},
+	}
 });
 
 
 $('.slider__btn--prev').click(function() {
-    
-    owl.trigger('prev.owl.carousel');
+	
+	owl.trigger('prev.owl.carousel');
 })
 
 $('.slider__btn--next').click(function() {
-    owl.trigger('next.owl.carousel');
+	owl.trigger('next.owl.carousel');
 })
 
 //nav-icon
@@ -25,7 +43,7 @@ const menuIcon = document.querySelector('.menu-icon');
 
 
 navBtn.onclick = function() {
-    nav.classList.toggle('nav--mobile');
+	nav.classList.toggle('nav--mobile');
 	menuIcon.classList.toggle('menu-icon--active');
-    document.body.classList.toggle('no-scroll');
+	document.body.classList.toggle('no-scroll');
 };
