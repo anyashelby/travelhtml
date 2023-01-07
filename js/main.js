e4597e2c0dd485e620a27b4d59d828f5a7a9eefe
Andrey <andreyhlvn@gmail.com>
2023-01-07 01:48:04 +0100

    320px responsive design

--- js/main.js
+++ js/main.js
@@ -4,16 +4,13 @@ owl.owlCarousel({
 	center: true,
 	loop: true,
 	margin: 30,
-	startPosition: 1,
-	items: 3,
+	startPosition: 0,
+	items: 1,
 	responsive : {
 		
-		0 : {
-			
-		},
-		
-		480 : {
-			
+		850 : {
+			items: 3,
+			startPosition: 1,	
 		},
 		
 		1000 : {
